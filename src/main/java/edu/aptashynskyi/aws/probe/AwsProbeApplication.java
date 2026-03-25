60e3e63672613b8fc59a0860aa20ef2992fb4318
Andrii Ptashynskyi <andrii.ptashynskyi@gmail.com>
2026-03-25 18:31:06 +0200

    generate spring boot project

---
+++ src/main/java/edu/aptashynskyi/aws/probe/AwsProbeApplication.java
@@ -0,0 +1,15 @@
+package edu.aptashynskyi.aws.probe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProbeApplication
+{
+
+    public static void main( String[] args )
+    {
+        SpringApplication.run( AwsProbeApplication.class, args );
+    }
+
+}
---
+++ src/test/java/edu/aptashynskyi/aws/probe/AwsProbeApplicationTests.java
@@ -0,0 +1,17 @@
+package edu.aptashynskyi.aws.probe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import( TestcontainersConfiguration.class )
+@SpringBootTest
+class AwsProbeApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
