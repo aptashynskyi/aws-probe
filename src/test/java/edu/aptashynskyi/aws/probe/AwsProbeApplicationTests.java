c56fcc07c3878450d8e27671693145a44141fdd7
Andrii Ptashynskyi <andrii.ptashynskyi@gmail.com>
2026-03-26 17:44:17 +0200

    make the AwsProbeApplicationTests pass

--- src/test/java/edu/aptashynskyi/aws/probe/AwsProbeApplicationTests.java
+++ src/test/java/edu/aptashynskyi/aws/probe/AwsProbeApplicationTests.java
@@ -1,9 +1,7 @@
 package edu.aptashynskyi.aws.probe;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
 @SpringBootTest
 class AwsProbeApplicationTests
@@ -12,7 +10,6 @@ class AwsProbeApplicationTests
     @Test
     void contextLoads()
     {
-        Assertions.assertNotNull(null);
     }
 
 }
